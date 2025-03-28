460530cf3195655e140efa90801a739ed86d0140
Ankush Jaiswal <ankushjaiswal1111@gmail.com>
2025-03-29 02:21:10 +0530

    update base url

--- src/Components/utils/config.js
+++ src/Components/utils/config.js
@@ -1,5 +1,5 @@
 // for prod 
 // const REACT_APP_BASE_URL = "https://dashboardbackend-8buf.onrender.com";
 // TODO : use it for local development 
-const REACT_APP_BASE_URL = "http://localhost:5100";
+const REACT_APP_BASE_URL = "https://dashboardbackend-du7b.onrender.com";
 export default REACT_APP_BASE_URL;
